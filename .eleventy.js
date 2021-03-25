9dcdf598ea5875dda946a4a5e3b5cb5a4b7b107c
Kevin Gimbel <hallo@kevingimbel.com>
2021-03-25 13:35:27 +0100

    fix: remove conditional loading as it didn't work as expected

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,12 @@
 module.exports = (eleventyConfig, options) => {
-  // control if mermaid graphs exist on page or not
-  let has_mermaid_graphs = false;
-
   eleventyConfig.addShortcode("mermaid_js", () => {
     let src = options?.mermaid_js_src || 'https://unpkg.com/mermaid/dist/mermaid.min.js';
-    if (has_mermaid_graphs) {
-      // reset variable after rendering mermaid-js
-      has_mermaid_graphs = false;
-      return `<script async src="${src}">mermaid.initialize({ startOnLoad: true });</script>`;
-    }
-    return "";
+    return `<script async src="${src}">mermaid.initialize({ startOnLoad: true });</script>`;
   });
 
   const highlighter = eleventyConfig.markdownHighlighter;
   eleventyConfig.addMarkdownHighlighter((str, language) => {
     if (language === "mermaid") {
-      // ensure JS code will be loaded
-      has_mermaid_graphs = true;
       return `<pre class="mermaid">${str}</pre>`;
     }
     if (highlighter) {
